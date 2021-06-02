8e1cd090587f5e7ba08ebb0fb5e2ceac4b715dd5
Melissa Flynn <mflynn814@gmail.com>
2021-06-02 16:37:23 -0700

    Added user profile picture to user sidebar. Minimal styling applied

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,2 @@
 json.extract! user, :id, :first_name, :last_name, :email, :breed, :location, :about_me
+json.profilePhotoUrl url_for(user.profile_photo)
--- frontend/components/home_page/user_sidebar/user_sidebar.jsx
+++ frontend/components/home_page/user_sidebar/user_sidebar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const UserSidebar = (props) => (
   <div className="user-sidebar">
-    <p>{props.user.firstName} {props.user.lastName}</p>
+    <p className="profile-photo"><img src={props.user.profilePhotoUrl} /></p>
+    <Link to={`/users/${props.user.id}`}>{props.user.firstName} {props.user.lastName}</Link>
     <p>{props.user.breed}</p>
   </div>
 )
