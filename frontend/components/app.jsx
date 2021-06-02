7ff928948899ab956f3d18b1163be2a08f733e9b
Melissa Flynn <mflynn814@gmail.com>
2021-06-02 09:51:48 -0700

    Added switch router statement to handle extraneous routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePage from './home_page/home_page';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { LogoutAuthRoute, LoginAuthRoute } from '../util/auth_util';
 import LoginContainer from './session_form/login_container';
 import SignupContainer from './session_form/signup_container';
@@ -9,10 +9,12 @@ import { validateEmail } from "../util/session_util";
 
 const App = () => (
   <div>
-    <LogoutAuthRoute exact path="/" component={HomePage} />
-    <LoginAuthRoute path="/feed" component={FeedContainer} />
-    <LogoutAuthRoute path="/login" component={LoginContainer} />
-    <LogoutAuthRoute path="/signup" component={() => <SignupContainer validateEmail={validateEmail}/>} />
+    <Switch>
+      <LoginAuthRoute path="/feed" component={FeedContainer} />
+      <LogoutAuthRoute path="/login" component={LoginContainer} />
+      <LogoutAuthRoute path="/signup" component={() => <SignupContainer validateEmail={validateEmail}/>} />
+      <LogoutAuthRoute path="/" component={HomePage} />
+    </Switch>
   </div>
 );
 
