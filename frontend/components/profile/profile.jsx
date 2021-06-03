d3f2aa120e9269c416bfd788a94f90161d43efdc
Melissa Flynn <mflynn814@gmail.com>
2021-06-03 14:24:01 -0700

    Solved issue for fetching user when loading profile

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,35 +1,72 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import HeaderContainer from '../header/header_container';
 import ContactInfoModal from '../modal/contact_info_modal';
 
 class Profile extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      user: undefined,
+      redirect: false,
+    }
+  }
+
+  updateUser () {
+    this.props.fetchUser(this.props.userId)
+      .then(
+        (response) => this.setState({
+          user: response.user
+        }),
+        () => this.setState({
+          redirect: true
+        })
+      )
+  }
+
+  componentDidUpdate () {
+    if (!this.state.redirect) {
+      if (!this.state.user || this.props.userId !== this.state.user.id.toString()) {
+        this.updateUser();
+      }
+    }
+  }
 
   componentDidMount () {
-    this.props.fetchUser(this.props.userId);
+    this.updateUser();
   }
 
   render () {
-    return (
-      <div className="profile-page">
-        {this.props.modal ? <ContactInfoModal user={this.props.user} currentUser={this.props.currentUser} updateModal={this.props.updateModal}/> : ''}
-        <HeaderContainer />
-        <div className="profile">
-          <section>
-            <div className="background">
-              <div></div>
-              <div></div>
+      console.log(this.state);
+      console.log(this.props);
+      if (this.state.redirect) {
+        return <Redirect to="/" />;
+      } else {
+        return (
+          <div className="profile-page">
+            {this.props.modal ? <ContactInfoModal user={this.state.user} currentUser={this.props.currentUser} updateModal={this.props.updateModal}/> : ''}
+            <HeaderContainer />
+            <div className="profile">
+              <section>
+                <div className="background">
+                  <div></div>
+                  <div></div>
+                </div>
+                <div>
+                  <img src={this.state.user ? this.state.user.profilePhotoUrl : ''} />
+                  <h3>{this.state.user ? this.state.user.firstName : ''} {this.state.user ? this.state.user.lastName : ''}</h3>
+                  <h4>{this.state.user ? this.state.user.breed : ''}</h4>
+                  <h5>{this.state.user ? this.state.user.location : ''}<p>•</p><p onClick={this.props.updateModal}>Contact info</p></h5>
+                </div>
+              </section>
             </div>
-            <div>
-              <img src={this.props.user ? this.props.user.profilePhotoUrl : ""} />
-              <h3>{this.props.user ? this.props.user.firstName : ""} {this.props.user ? this.props.user.lastName : ""}</h3>
-              <h4>{this.props.user ? this.props.user.breed : ""}</h4>
-              <h5>{this.props.user ? this.props.user.location : ""}<p>•</p><p onClick={this.props.updateModal}>Contact info</p></h5>
-            </div>
-          </section>
-        </div>
-
-      </div>
-    )
+    
+          </div>
+        )
+      }
+    // } else {
+    //   return null;
+    // }
   }
 }
 
