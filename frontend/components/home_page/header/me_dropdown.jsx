9c0f18ef4f4facc4ccd59f83cf8668c2f7773220
Melissa Flynn <mflynn814@gmail.com>
2021-06-02 18:01:23 -0700

    Updated dropdown to use react state trigger. Need to solve for leaving dropdown open when clicking into itself

--- frontend/components/home_page/header/me_dropdown.jsx
+++ frontend/components/home_page/header/me_dropdown.jsx
@@ -3,13 +3,37 @@ import React from 'react';
 class MeDropdown extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      showDropdown: false
+    }
     this.dropdownDisplay = this.dropdownDisplay.bind(this);
+    this.dropdownHide = this.dropdownHide.bind(this);
   }
   
+  componentDidUpdate () {
+
+    setTimeout(() => {
+      if (this.state.showDropdown) {
+        window.addEventListener('click', this.dropdownHide)
+      } else {
+        window.removeEventListener('click', this.dropdownHide)
+      }
+    }, 0)
+
+  }
+
   dropdownDisplay (e) {
     e.preventDefault();
-    const dropdown = document.getElementById("me-dropdown");
-    dropdown.classList.add('shown');
+    this.setState({
+      showDropdown: true
+    })
+  }
+
+  dropdownHide (e) {
+    e.preventDefault();
+    this.setState({
+      showDropdown: false
+    })
   }
 
   render () {
@@ -22,11 +46,14 @@ class MeDropdown extends React.Component {
             <p></p>
           </div>
         </div>
-        <div id="me-dropdown">
-          <p>{this.props.user.firstName} {this.props.user.lastName}</p>
-          <p>{this.props.user.breed}</p>
-          <button onClick={this.props.logout}>Sign Out</button>
-        </div>
+        {this.state.showDropdown ? (
+          <div id="me-dropdown" onClick={(e) => e.stopPropogation()}>
+            <p>{this.props.user.firstName} {this.props.user.lastName}</p>
+            <p>{this.props.user.breed}</p>
+            <button onClick={this.props.logout}>Sign Out</button>
+          </div>
+        ) : ''
+        }
       </div>
     )
   }
