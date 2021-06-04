728421103ae03cf53bd4b0a63da88c5d892636a0
Melissa Flynn <mflynn814@gmail.com>
2021-06-03 21:40:18 -0700

    Seeded databse with profile photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,14 @@
 
 User.destroy_all
 
-demo_user = User.create(email: 'demodog@dogs.com', password: 'password', first_name: 'Demo', last_name: 'Dog', breed: "Man's best friend");
-users = User.create([{email: 'goose@dogs.com', password: 'password', first_name: 'Goose', last_name: 'Flynn', breed: 'Coonhound Mix'}, {email: 'duke@dogs.com', password: 'password', first_name: 'Duke', last_name: 'Flynn', breed: 'Labrador Retriever'}, {email: 'koby@dogs.com', password: 'password', first_name: 'Koby', last_name: 'Albert', breed: 'Australian Shepherd'}])
+require 'open-uri'
+
+demo_user = User.create(email: 'demodog@dogs.com', password: 'password', first_name: 'Demo', last_name: 'Dog', breed: "Man's best friend", country: 'Turkey', region: 'Istanbul');
+
+demo_user.profile_photo.attach(io: URI.open('https://barked-in-seeds.s3-us-west-1.amazonaws.com/demo_dog_prof.jpg'), filename: 'demo_dog_prof.jpg')
+
+users = User.create([{email: 'goose@dogs.com', password: 'password', first_name: 'Goose', last_name: 'Flynn', breed: 'Coonhound Mix', country: 'United States', region: 'San Francisco, California'}, {email: 'duke@dogs.com', password: 'password', first_name: 'Duke', last_name: 'Flynn', breed: 'Labrador Retriever', country: 'United States', region: 'Charleston, South Carolina'}, {email: 'koby@dogs.com', password: 'password', first_name: 'Koby', last_name: 'Albert', breed: 'Australian Shepherd', country: 'United States', region: 'San Diego, California'}])
+
+users[0].profile_photo.attach(io: URI.open('https://barked-in-seeds.s3-us-west-1.amazonaws.com/Goose_prof.jpg'), filename: 'Goose_prof.jpg')
+users[1].profile_photo.attach(io: URI.open('https://barked-in-seeds.s3-us-west-1.amazonaws.com/Duke_prof.jpg'), filename: 'Duke_prof.jpg')
+users[2].profile_photo.attach(io: URI.open('https://barked-in-seeds.s3-us-west-1.amazonaws.com/Koby_prof.jpeg'), filename: 'Koby_prof.jpeg')
