5105bb88b8be917495897e4cfb37ceaed7dc572a
Melissa Flynn <mflynn814@gmail.com>
2021-06-04 18:21:26 -0700

    Added logic to only include logger middleware outside of production environment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,6 +3,13 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+let myMiddlewares;
+if (process.env.NODE_ENV === "production") {
+  myMiddlewares = thunk;
+} else {
+  myMiddlewares = [thunk, logger];
+}
+
 export const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(rootReducer, preloadedState, applyMiddleware(...myMiddlewares))
 );
